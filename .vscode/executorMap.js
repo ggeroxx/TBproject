50216f97c903e55a08c892c096bee6c8010a3e7b
Ale-Biagio <a.dellanna@studenti.unibs.it>
2024-02-26 15:21:26 +0100

    configurazione per MacOs e Win32

---
+++ .vscode/executorMap.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+const isWindows = process.platform === 'win32';
+
+const executorMap = {
+        "java": isWindows
+            ? "cd $dir && cd ../../.vscode && node executorMap.js && cd $dir && javac -cp .;..\\lib\\mysql-connector-j-8.3.0.jar -d ..\\bin\\ $fileName && java -cp .;..\\lib\\mysql-connector-j-8.3.0.jar;..\\bin Main"
+            : "cd $dir && cd ../../.vscode && node executorMap.js && cd $dir && javac -cp .:../lib/mysql-connector-j-8.3.0.jar -d ../bin/ $fileName && java -cp .:../lib/mysql-connector-j-8.3.0.jar:../bin Main"
+};
+
+const additionalProperties = {
+    "java.project.sourcePaths": ["src"],
+    "java.project.outputPath": "bin",
+    "java.project.referencedLibraries": [
+        "lib/**/*.jar"
+    ]
+};
+
+const finalConfig = {
+    ...{ "code-runner.executorMap": executorMap },
+    ...additionalProperties
+};
+
+const jsonData = JSON.stringify(finalConfig, null, 2);
+
+fs.writeFileSync('settings.json', jsonData, 'utf-8');
