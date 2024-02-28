34bd7f544447a3560a14a18e15333efe6e2bfbd1
ggeroxx <davidegeroldi337@gmail.com>
2024-02-28 12:00:17 +0100

    second version

--- src/Main.java
+++ src/Main.java
@@ -14,10 +14,11 @@ public class Main
 
         Scanner scanner = new Scanner( System.in );
         Console console = System.console();
-        String scelta;
+        String choice;
 
         Conn.openConnection();
 
+        clearConsole(0);
         do 
         {
             System.out.print(   
@@ -28,14 +29,22 @@ public class Main
                                 " 1.  login\n" +
                                 " 2.  exit\n" +
                                 "--------------\n" +
-                                "Inserisci la scelta (1/2) --> "
+                                "Enter yout choice (1/2) --> "
                             );
-            scelta = scanner.nextLine();
+            choice = scanner.nextLine();
 
-            switch(scelta) 
+            switch ( choice ) 
             {
                 case "1":
-                        System.out.print("\nInserisci username: ");
+                        clearConsole(100);
+                        System.out.print(
+                                            "\n" +
+                                            "-----------\n" +
+                                            "LOGIN\n" +
+                                            "-----------\n"
+                                        );
+
+                        System.out.print("inserisci username: ");
                         String username = scanner.nextLine();
                         
                         char[] passwordChars = console.readPassword( "Inserisci password: " );
@@ -44,8 +53,9 @@ public class Main
                         Configurator conf = new Configurator( username, password );
 
                         if ( !conf.login() )
-                        {
+                        {   
                             System.out.println("\n --- ATTENZIONE, username e/o password errati! Riprova! ---");
+                            clearConsole(2500);
                             break;
                         }
 
@@ -53,15 +63,23 @@ public class Main
                         {
                             String newUsername, newPassword;
                             boolean next = false, otherNext = false;
+
+                            clearConsole(200);
+                            System.out.print(
+                                                "\n" +
+                                                "--------------\n" +
+                                                "REGISTRATION\n" +
+                                                "--------------\n"
+                                            );
+
                             do
                             {
-                                System.out.println();
                                 System.out.print("Inserisci un nuovo username (min 3 max 20 caratteri): ");
                                 newUsername = scanner.nextLine();
                                 next = conf.attendedUsername( newUsername );
                                 if ( next ) System.out.println("\n --- username NON disponibile ! --- \n");
                                 otherNext = conf.checkPatternUsername( newUsername );
-                                if ( !otherNext ) System.out.println( "\n --- ATTENZIONE, parametri non rispettati! Minimo 3 e massimo 20 caratteri --- \n" );
+                                if ( !otherNext ) System.out.print( "\n --- ATTENZIONE, parametri non rispettati! Minimo 3 e massimo 20 caratteri --- \n" );
                             } while ( next || !otherNext );
                             do
                             {   
@@ -73,17 +91,72 @@ public class Main
                             
                             conf.changeCredentials( newUsername, newPassword );
                         }
+                        clearConsole(1000);
+                        configuratorOtions( scanner );
                     break;
                 case "2":
-                        System.out.println("\nArrivederci...\n\n");
+                        System.out.println("\nBye bye ...\n\n");
                     break;
                 default:
-                        System.out.println("\n --- Opzione non valida ! ---");
+                        System.out.println("\n --- Invalid option ! ---");
+                        clearConsole(2000);
                     break;
             }
-        } while ( !scelta.equals("2") );
+        } while ( !choice.equals("2") );
         
         Conn.closeConnection();
         scanner.close();
+    }
+
+    public static void clearConsole ( int millis ) 
+    {
+        try 
+        {
+            Thread.sleep(millis);
+            if ( System.getProperty("os.name").contains("Windows") )
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            else
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+        } 
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void configuratorOtions ( Scanner scanner )
+    {
+        String choice;
+        
+        do
+        {
+            System.out.print(
+                                "\n" +
+                                "--------------\n" +
+                                "OPTIONS\n" +
+                                "--------------\n" +
+                                " 1.  insert new district\n" +
+                                " 2.  view district\n" + 
+                                " 9.  logout\n" +
+                                "--------------\n" +
+                                "Enter your choice (1/9) --> "
+                            );
+            choice = scanner.nextLine();
+
+            switch ( choice )
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "9":
+                        clearConsole(1000);
+                    break;
+                default:
+                        System.out.println("\n --- Invalid option ! ---");
+                        clearConsole(2000);
+                    break;
+            }
+        } while ( !choice.equals("9") );
     }
 }
